ca4673e4c4c086abb88759b8e49f9069bee02aa7
Diogo Lessa <diogo.r.lessa@gmail.com>
2024-05-16 09:23:57 -0300

    Changed from local .csv to download, minor fixes for local/docker build

--- swagger_server/controllers/default_controller.py
+++ swagger_server/controllers/default_controller.py
@@ -1,6 +1,8 @@
+import os
 import connexion
 import six
 import pandas as pd
+import requests
 
 from swagger_server.models.comercializacao_item import ComercializacaoItem  # noqa: E501
 from swagger_server.models.exportacao_item import ExportacaoItem  # noqa: E501
@@ -9,6 +11,20 @@ from swagger_server.models.processamento_item import ProcessamentoItem  # noqa: 
 from swagger_server.models.producao_item import ProducaoItem  # noqa: E501
 from swagger_server import util
 
+
+def download_csv(url):
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        file_name = url.split('/')[-1]
+        save_path = os.path.join("data/", file_name)
+
+        with open(save_path, 'wb') as file:
+            file.write(response.content)
+    
+        return save_path
+    else:
+        print("Error: " + str(response.status_code))
 
 def search_comercializacao(comercializacao_product_search_string, ano_search_string=None):  # noqa: E501
     """searches comercializacao
@@ -27,39 +43,36 @@ def search_comercializacao(comercializacao_product_search_string, ano_search_str
 
     result = []
 
-    data = pd.read_csv('data/Comercio.csv', skipinitialspace=True, sep=';')
-    
-    try:
-        all_product_info = data[data['Produto'] == comercializacao_product_search_string]
+    file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/Comercio.csv")
+    data = pd.read_csv(file, sep=';')
+        
+    all_product_info = data[data['Produto'] == comercializacao_product_search_string]
 
-        if ano_search_string is None:
-            for index, year in enumerate(all_product_info):
-                if index >= 3:
-                    category = all_product_info['Categoria'].values[0]
-                    quantity = all_product_info[year].values[0]
+    if ano_search_string is None:
+        for index, year in enumerate(all_product_info):
+            if index >= 3:
+                product = all_product_info['Produto'].values[0]
+                quantity = all_product_info[year].values[0]
 
-                    product_info = ComercializacaoItem(categoria=category,
+                item = ComercializacaoItem(categoria=product,
                                         cultivar=comercializacao_product_search_string,
                                         ano=int(year),
                                         quantidade=int(quantity)
                                         ).to_dict()
 
-                    result.append(product_info)
-        else:
-            category = all_product_info['Categoria'].values[0]
-            quantity = all_product_info[ano_search_string].values[0]
-            
-            product_info = ComercializacaoItem(categoria=category,
+                result.append(item)
+    else:
+        product = all_product_info['Produto'].values[0]
+        quantity = all_product_info[ano_search_string].values[0]
+                
+        item = ComercializacaoItem(categoria=product,
                                         cultivar=comercializacao_product_search_string,
                                         ano=int(ano_search_string),
                                         quantidade=int(quantity)
                                         ).to_dict()
 
-            result.append(product_info)
-    except:
-        return ['Product or Product/year not found']
-    else:
-        return result
+        result.append(item)
+    return result
 
 
 def search_exportacao(exportacao_category_search_string, exportacao_country_search_string, ano_search_string=None):  # noqa: E501
@@ -79,51 +92,49 @@ def search_exportacao(exportacao_category_search_string, exportacao_country_sear
 
     result = []
 
-    try:
-        match exportacao_category_search_string:
-            case 'Vinhos de mesa':
-                data = pd.read_csv('data/Exp/ExpVinho.csv', sep=';')
-            case 'Espumantes':
-                data = pd.read_csv('data/Exp/ExpEspumantes.csv', sep=';')
-            case 'Uvas frescas':
-                data = pd.read_csv('data/Exp/ExpUva.csv', sep=';')
-            case 'Suco de uva':
-                data = pd.read_csv('data/Exp/ExpSuco.csv', sep=';')
+    match exportacao_category_search_string:
+        case 'Vinhos de mesa':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ExpVinho.csv")
+        case 'Espumantes':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ExpEspumantes.csv")
+        case 'Uvas frescas':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ExpUva.csv")
+        case 'Suco de uva':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ExpSuco.csv")
+            
+    data = pd.read_csv(file, sep=';')
 
-        all_country_info = data[data['País'] == exportacao_country_search_string]
+    all_country_info = data[data['País'] == exportacao_country_search_string]
 
-        if ano_search_string is None:
-            for index, year in enumerate(all_country_info):
-                if index >= 2:
-                    if ".1" not in year:
-                        quantity = all_country_info[year].values[0]
-                        value = all_country_info[year + '.1'].values[0]
+    if ano_search_string is None:
+        for index, year in enumerate(all_country_info):
+            if index >= 2:
+                if ".1" not in year:
+                    quantity = all_country_info[year].values[0]
+                    value = all_country_info[year + '.1'].values[0]
 
-                        country_info = ExportacaoItem(pais=exportacao_country_search_string,
+                    item = ExportacaoItem(pais=exportacao_country_search_string,
                                         ano=int(year),
                                         quantidade=int(quantity),
                                         valor=int(value)
                                         ).to_dict()
 
-                        result.append(country_info)
-        else:
-            quantity = all_country_info[ano_search_string].values[0]
+                    result.append(item)
+    else:
+        quantity = all_country_info[ano_search_string].values[0]
             
-            if ".1" not in ano_search_string:
-                quantity = all_country_info[ano_search_string].values[0]
-                value = all_country_info[ano_search_string + '.1'].values[0]
+        if ".1" not in ano_search_string:
+            quantity = all_country_info[ano_search_string].values[0]
+            value = all_country_info[ano_search_string + '.1'].values[0]
 
-                country_info = ExportacaoItem(pais=exportacao_country_search_string,
+            item = ExportacaoItem(pais=exportacao_country_search_string,
                                         ano=int(ano_search_string),
                                         quantidade=int(quantity),
                                         valor=int(value)
                                         ).to_dict()
 
-                result.append(country_info)
-    except:
-        return ['Category/country or Category/country and year not found']
-    else:
-        return result
+            result.append(item)
+    return result
 
 
 def search_importacao(importacao_category_search_string, importacao_country_search_string, ano_search_string=None):  # noqa: E501
@@ -143,53 +154,51 @@ def search_importacao(importacao_category_search_string, importacao_country_sear
 
     result = []
 
-    try:
-        match importacao_category_search_string:
-            case 'Vinhos de mesa':
-                data = pd.read_csv('data/Imp/ImpVinhos.csv', sep=';')
-            case 'Espumantes':
-                data = pd.read_csv('data/Imp/ImpEspumantes.csv', sep=';')
-            case 'Uvas frescas':
-                data = pd.read_csv('data/Imp/Frescas.csv', sep=';')
-            case 'Uvas passas':
-                data = pd.read_csv('data/Imp/ImpPassas.csv', sep=';')
-            case 'Suco de uva':
-                data = pd.read_csv('data/Imp/ImpSuco.csv', sep=';')
+    match importacao_category_search_string:
+        case 'Vinhos de mesa':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ImpVinhos.csv")
+        case 'Espumantes':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ImpEspumantes.csv")
+        case 'Uvas frescas':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ImpFrescas.csv")
+        case 'Uvas passas':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ImpPassas.csv")
+        case 'Suco de uva':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ImpSuco.csv")
 
-        all_country_info = data[data['País'] == importacao_country_search_string]
+    data = pd.read_csv(file, sep=';')
 
-        if ano_search_string is None:
-            for index, year in enumerate(all_country_info):
-                if index >= 2:
-                    if ".1" not in year:
-                        quantity = all_country_info[year].values[0]
-                        value = all_country_info[year + '.1'].values[0]
-
-                        country_info = ImportacaoItem(pais=importacao_country_search_string,
+    all_country_info = data[data['País'] == importacao_country_search_string]
+    
+    if ano_search_string is None:
+        for index, year in enumerate(all_country_info):
+            if index >= 2:
+                if ".1" not in year:
+                    quantity = all_country_info[year].values[0]
+                    value = all_country_info[year + '.1'].values[0]
+                    
+                    item = ImportacaoItem(pais=importacao_country_search_string,
                                         ano=int(year),
                                         quantidade=int(quantity),
                                         valor=int(value)
                                         ).to_dict()
 
-                        result.append(country_info)
-        else:
-            quantity = all_country_info[ano_search_string].values[0]
-            
-            if ".1" not in ano_search_string:
-                quantity = all_country_info[ano_search_string].values[0]
-                value = all_country_info[ano_search_string + '.1'].values[0]
+                    result.append(item)
+    else:
+        quantity = all_country_info[ano_search_string].values[0]
 
-                country_info = ImportacaoItem(pais=importacao_country_search_string,
+        if ".1" not in ano_search_string:
+            quantity = all_country_info[ano_search_string].values[0]
+            value = all_country_info[ano_search_string + '.1'].values[0]
+
+            item = ImportacaoItem(pais=importacao_country_search_string,
                                         ano=ano_search_string,
                                         quantidade=int(quantity),
                                         valor=int(value)
                                         ).to_dict()
 
-                result.append(country_info)
-    except:
-        return ['Category/country or Category/country and year not found']
-    else:
-        return result
+            result.append(item)
+    return result
 
 
 def search_processamento(processamento_category_search_string, processamento_product_search_string, ano_search_string=None):  # noqa: E501
@@ -209,53 +218,51 @@ def search_processamento(processamento_category_search_string, processamento_pro
 
     result = []
 
-    try:
-        match processamento_category_search_string:
-            case 'Viníferas':
-                data = pd.read_csv('data/Processa/ProcessaViniferas.csv', sep='\t')
-            case 'Americanas e Híbridas':
-                data = pd.read_csv('data/Processa/ProcessaAmericanas.csv', sep='\t')
-            case 'Uvas de mesa':
-                data = pd.read_csv('data/Processa/ProcessaMesa.csv', sep='\t')
-            case 'Sem classificação':
-                data = pd.read_csv('data/Processa/ProcessaSemclass.csv', sep='\t')
+    match processamento_category_search_string:
+        case 'Viníferas':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ProcessaViniferas.csv")
+        case 'Americanas e Híbridas':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ProcessaAmericanas.csv")
+        case 'Uvas de mesa':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ProcessaMesa.csv")
+        case 'Sem classificação':
+            file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/ProcessaSemclass.csv")
+
+    data = pd.read_csv(file, sep='\t')
         
-        all_crop_info = data[data['cultivar'] == processamento_product_search_string]
+    all_crop_info = data[data['cultivar'] == processamento_product_search_string]
 
-        if ano_search_string is None:
-            for index, year in enumerate(all_crop_info):
-                if index >= 3:
-                    crop = all_crop_info['cultivar'].values[0]
-                    quantity = all_crop_info[year].values[0]
+    if ano_search_string is None:
+        for index, year in enumerate(all_crop_info):
+            if index >= 3:
+                crop = all_crop_info['cultivar'].values[0]
+                quantity = all_crop_info[year].values[0]
 
-                    if isinstance(quantity, str) == True:
-                        continue
-                    else:
-                        product_info = ProcessamentoItem(categoria=processamento_category_search_string,
+                if isinstance(quantity, str) == True:
+                    continue
+                else:
+                    item = ProcessamentoItem(categoria=processamento_category_search_string,
                                                         cultivar=crop,
                                                         ano=int(year),
                                                         quantidade=int(quantity)
                                                         ).to_dict()
 
-                        result.append(product_info)
-        else:
-            crop = all_crop_info['cultivar'].values[0]
-            quantity = all_crop_info[ano_search_string].values[0]
+                result.append(item)
+    else:
+        crop = all_crop_info['cultivar'].values[0]
+        quantity = all_crop_info[ano_search_string].values[0]
 
-            if isinstance(quantity, str) == True:
-                pass
-            else:
-                product_info = ProcessamentoItem(categoria=processamento_category_search_string,
+        if isinstance(quantity, str) == True:
+            pass
+        else:
+            item = ProcessamentoItem(categoria=processamento_category_search_string,
                                                 cultivar=crop,
                                                 ano=int(ano_search_string),
                                                 quantidade=int(quantity)
                                                 ).to_dict()
 
-                result.append(product_info)
-    except:
-        return ['Category/product or Category/product and year not found']
-    else:
-        return result
+            result.append(item)
+    return result
 
 
 def search_producao(produto_search_string, ano_search_string=None):  # noqa: E501
@@ -273,32 +280,30 @@ def search_producao(produto_search_string, ano_search_string=None):  # noqa: E50
 
     result = []
 
-    try:
-        data = pd.read_csv('data/Producao.csv', sep=';')
+    file = download_csv("http://vitibrasil.cnpuv.embrapa.br/download/Producao.csv")
+
+    data = pd.read_csv(file, sep=';')
         
-        all_product_info = data[data['produto'] == produto_search_string]
+    all_product_info = data[data['produto'] == produto_search_string]
 
-        if ano_search_string is None:
-            for index, year in enumerate(all_product_info):
-                if index >= 2:
-                    quantity = all_product_info[year].values[0]
+    if ano_search_string is None:
+        for index, year in enumerate(all_product_info):
+            if index >= 3:
+                quantity = all_product_info[year].values[0]
 
-                    product_info = ProducaoItem(produto=produto_search_string,
-                                        ano=int(year),
-                                        quantidade=int(quantity)
-                                        ).to_dict()
+                item = ProducaoItem(produto=produto_search_string,
+                                    ano=int(year),
+                                    quantidade=int(quantity)
+                                    ).to_dict()
 
-                    result.append(product_info)
-        else:
-            quantity = all_product_info[ano_search_string].values[0]
+                result.append(item)
+    else:
+        quantity = all_product_info[ano_search_string].values[0]
 
-            product_info = ProducaoItem(produto=produto_search_string,
+        item = ProducaoItem(produto=produto_search_string,
                                         ano=int(ano_search_string),
                                         quantidade=int(quantity)
                                         ).to_dict()
 
-            result.append(product_info)
-    except:
-        return ['Product or Product/year not found']
-    else:
-        return result
+        result.append(item)
+    return result
